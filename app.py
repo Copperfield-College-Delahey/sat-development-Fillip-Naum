b72aa438e2ac1f7332f8da500f5f7a2b85d30b91
Fillip-Naum <nau0020@copperfieldcollege.vic.edu.au>
2025-06-25 12:54:37 +1000

    Made Window

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 #Gui setup
+import customtkinter as ctk 
+from CTkTable import *
+from pages.searchPage import searchPage
+from pages.addPage import AddPage 
+
+
+
+app =  ctk.CTk()
+app.Title("Inventory System")
+app.geometry("1000x600")
+
+
