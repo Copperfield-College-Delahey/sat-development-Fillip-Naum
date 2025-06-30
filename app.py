46d7783bc166b8b6fef1c10a304de06f272b4d05
Fillip-Naum <nau0020@copperfieldcollege.vic.edu.au>
2025-06-30 13:32:49 +1000

    creating columns and rows

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 #Gui setup
 import customtkinter as ctk 
-from CTkTable import *
-from pages.searchPage import searchPage
-from pages.addPage import AddPage 
 
 
 
@@ -11,3 +8,10 @@ app.Title("Inventory System")
 app.geometry("1000x600")
 
 
+app.grid_columnconfigure(0, weight=1)
+app.grid_columnconfigure(1, weight=1)
+app.grid_columnconfigure(2, weight1)
+app.grid_rowconfigure(0, weight=1)
+
+
+app.mainloop()  
