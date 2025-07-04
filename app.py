76852ba562d96e4d18a27f7e69d7e4e0b5619b17
Fillip-Naum <nau0020@copperfieldcollege.vic.edu.au>
2025-07-04 10:12:34 +1000

    Update app.py
    Continuing window and starting labels

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ import customtkinter as ctk
 
 
 app =  ctk.CTk()
-app.Title("Inventory System")
+app.title("Inventory System")
 app.geometry("1000x600")
 
 
 app.grid_columnconfigure(0, weight=1)
 app.grid_columnconfigure(1, weight=1)
-app.grid_columnconfigure(2, weight1)
+app.grid_columnconfigure(2, weight=1)
 app.grid_rowconfigure(0, weight=1)
+app.grid_rowconfigure(1, weight=1)
+
+
+topFrame = ctk.CTkFrame(app, border_width=4)
+topFrame.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=5, pady=5)
+titleLabel = ctk.CTkLabel(topFrame, text="Inventory System", font=("Arial", 32))
+
+
 
 
 app.mainloop()  
