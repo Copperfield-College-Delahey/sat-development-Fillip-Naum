62d67a450efa6f4bb64d58e0755899549eaf65d4
Fillip-Naum <nau0020@copperfieldcollege.vic.edu.au>
2025-08-04 13:09:00 +1000

    Current Inventory window to have all the entry fields

--- app.py
+++ app.py
@@ -11,10 +11,16 @@ def show_frame(name):
         f.grid_forget()
     frames[name].grid(row=1, column=0, columnspan=2, sticky="nsew", padx=5, pady=5)
 
+def save(save):
+    
+
+
+#Top Frame
 topFrame = ctk.CTkFrame(app)
 topFrame.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=5, pady=5)
-ctk.CTkButton(topFrame, text="Inventory System", font=("Helvetica", 30) ,command=lambda: show_frame("main")).grid(row=0, column=1, padx=15, pady=15)
+ctk.CTkButton(topFrame, text="Inventory System", font=("Helvetica", 30) ,command=lambda: show_frame("main")).grid(row=0, column=2, padx=15, pady=15)
 
+#Mid Frame/Main Window
 midFrame = ctk.CTkFrame(app)
 midFrame.grid_columnconfigure((0,1,2), weight=1)
 ctk.CTkButton(midFrame, text="Restock", font=("Helvetica", 25, "bold"), command=lambda: show_frame("restock")).grid(row=0, column=0, padx=80, pady=80, sticky="nsew")
@@ -22,16 +28,31 @@ ctk.CTkButton(midFrame, text="Current Inventory", font=("Helvetica", 25, "bold")
 ctk.CTkButton(midFrame, text="Reports", font=("Helvetica", 25, "bold"), command=lambda: show_frame("reports")).grid(row=0, column=2, padx=80, pady=80, sticky="nsew")
 frames["main"] = midFrame
 
-inventory = ctk.CTkFrame(app)
-ctk.CTkLabel(inventory, text="Current Inventory", font=("Helvetica", 24)).pack(pady=20)
-ctk.CTkButton(inventory, text="Add New Inventory", fg_color="red").pack(pady=10)
-frames["inventory"] = inventory
 
+#Inventory Window
+inventory = ctk.CTkFrame(app)
+labels = [
+    "ItemId", "ItemName", "Quantity", "PricePerUnit",
+    "DataAdded", "SupplierName", "ItemColour", "ItemDescription"
+]
+entry_labels = {}
+
+for i, label in enumerate(labels):
+    ctk.CTkLabel(inventory, text=label + ":").grid(row=i, column=0, sticky="e", padx=10, pady=5)
+    entry=ctk.CTkEntry(inventory, width=300)
+    entry.grid(row=i, column=1, sticky="w", padx=10, pady=5)
+    entry_labels[label] = entry
+
+ctk.CTkButton(inventory, text="Save", fg_color="red").grid(row=len(labels), column=0, columnspan=2, pady=15)
+frames["inventory"]=inventory
+
+#Restock Window
 restock = ctk.CTkFrame(app)
 ctk.CTkLabel(restock, text="Manual Restock", font=("Helvetica", 20), text_color="blue").pack(pady=10)
 ctk.CTkLabel(restock, text="Automatic Restock", font=("Helvetica", 20), text_color="blue").pack(pady=10)
 frames["restock"] = restock
 
+#Reports Window
 reports = ctk.CTkFrame(app)
 ctk.CTkLabel(reports, text="Reports", font=("Helvetica", 24)).pack(pady=10)
 ctk.CTkLabel(reports, text="Item ID:").pack()
@@ -40,6 +61,7 @@ ctk.CTkLabel(reports, text="Item Name:").pack()
 ctk.CTkEntry(reports).pack()
 frames["reports"] = reports
 
+#Final Window Setup
 app.grid_rowconfigure(1, weight=1)
 app.grid_columnconfigure((0, 1), weight=1)
 show_frame("main")
